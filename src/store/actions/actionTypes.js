4be091156a92dea38260b9c4e55f5d7055e647ec
flukycoder <flukycoder@gmail.com>
2019-04-20 19:45:36 +0530

    Redux Added as same as on web

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,2 @@
+export const ADD_PLACE = "ADD_PLACE";
+export const SELECT_PLACE = "SELECT_PLACE";
---
+++ src/store/reducers/places.js
@@ -0,0 +1,12 @@
+let initialState = {
+  places: []
+};
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
