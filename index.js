9f129426552da8b6e8dc8ece0cbb773c22981470
flukycoder <flukycoder@gmail.com>
2019-04-21 12:50:50 +0530

    react native navigation fixed

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @format
  */
 import React from "react";
-import { AppRegistry } from "react-native";
+import { Navigation } from "react-native-navigation";
 import App from "./App";
-import { name as appName } from "./app.json";
 import { Provider } from "react-redux";
 import configureStore from "./src/store/configureStore";
 const store = configureStore();
@@ -13,4 +12,22 @@ const appMain = () => (
     <App />
   </Provider>
 );
-AppRegistry.registerComponent(appName, () => appMain);
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => appMain);
+
+// Navigation.startSingleScreenApp({
+//   screen: {
+//     screen: "navigation.playground.WelcomeScreen",
+//     title: "Login"
+//   }
+// });
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
